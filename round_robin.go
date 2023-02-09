ee0e2ad436fffc709d7c82fcfdb9e2c7a23b5970
Safeer Ansari <30790884+thegeekyasian@users.noreply.github.com>
2023-02-10 02:15:20 +0400

    Refactor for a simplified implementation

--- round_robin.go
+++ round_robin.go
@@ -9,29 +9,30 @@ import (
 // ErrorNoObjectsProvided is the error that occurs when no objects are provided.
 var ErrorNoObjectsProvided = errors.New("no objects provided")
 
-// RoundRobin is an interface for representing round-robin balancing.
-type RoundRobin[O any] interface {
-	Next() *O
-}
-
-type roundrobin[O any] struct {
+type RoundRobin[O any] struct {
 	objects []*O
 	next    uint32
 }
 
 // New returns RoundRobin implementation with roundrobin.
-func New[O any](objects ...*O) (RoundRobin[O], error) {
+func New[O any](objects ...*O) (*RoundRobin[O], error) {
 	if len(objects) == 0 {
 		return nil, ErrorNoObjectsProvided
 	}
 
-	return &roundrobin[O]{
+	return &RoundRobin[O]{
 		objects: objects,
 	}, nil
 }
 
 // Next returns the next object.
-func (r *roundrobin[O]) Next() *O {
+func (r *RoundRobin[O]) Next() *O {
 	n := atomic.AddUint32(&r.next, 1)
+
+	if int(n) > len(r.objects) {
+		atomic.StoreUint32(&r.next, 0)
+		n = 1
+	}
 	return r.objects[(int(n)-1)%len(r.objects)]
 }
+
